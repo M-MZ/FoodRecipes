5551350e6584e6d32819fd15adeb6275cdd2533e
MMZ <mmz11493@gmail.com>
2019-10-24 19:08:29 +0800

    add-food2fork-api

---
+++ app/src/main/java/com/mmz/foodrecipes/util/Constants.java
@@ -0,0 +1,11 @@
+package com.mmz.foodrecipes.util;
+
+/**
+ * Created by Mahmoud Mahmoudzadeh on 10/23/2019.
+ */
+public class Constants {
+
+    public static final String BASE_URL = "https//www.food2fork.com";
+
+    public static final String API_KEY = "03e96fae67102967f22fb4fceab363c7 ";
+}
